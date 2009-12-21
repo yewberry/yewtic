f400be1070ecf57692dd93009cd622ece0082f75
yew1998 <yew1998@5ddc4e96-dffd-11de-b4b3-6d349e4f6f86>
2009-12-21 08:24:14 +0000

    add logger and webkit

---
+++ trunk/ytk/BatDown/WebBrowser.h
@@ -0,0 +1,43 @@
+#ifndef WEBBROWSER_H
+#define WEBBROWSER_H
+
+#include "common.h"
+#include <QtGui/QWidget>
+
+class QWebView;
+class QLineEdit;
+class QPushButton;
+
+class WebBrowser : public QWidget
+{
+	Q_OBJECT
+
+public:
+	WebBrowser(QWidget *parent = 0, Qt::WFlags flags = 0);
+	~WebBrowser(void);
+
+
+public slots:
+	void populateJavaScriptWindowObject();
+	void setValues(const QString &value);
+
+protected slots:
+	void finishLoading(bool);
+
+private slots:
+	void evalJS();
+
+private:
+	void initLogger();
+	Logger logger;
+
+private:
+	QString extjs;
+	QWebView *view;
+	QLineEdit *locationEdit;
+
+	QPushButton *gooBtn;
+};
+
+
+#endif // WEBBROWSER_H
