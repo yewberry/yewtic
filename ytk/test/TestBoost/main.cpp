6f320e990c627de10e9e8922672dbfbabbcb9088
yew1998 <yew1998@5ddc4e96-dffd-11de-b4b3-6d349e4f6f86>
2010-06-28 13:27:37 +0000

    Boost.Property not ok

--- ytk/test/TestBoost/main.cpp
+++ ytk/test/TestBoost/main.cpp
@@ -1,11 +1,28 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
-#include <iostream> 
+#include <iostream>
+
+//S test Boost.PropertyTree
+#include "Boost.PropertyTree.cpp"
+//E
 
 using namespace std; 
 using namespace boost::posix_time;
 using namespace boost::gregorian;
 
+void testDatetime();
+void testPropertyTree();
+
 int main() {
+	testDatetime();
+	testPropertyTree();
+
+	cin.get();
+	return 0;
+}
+
+void testDatetime(){
+	cout<<"[START TEST Datetime]\n";
+
 	ptime t(second_clock::local_time());
 	string s = to_iso_extended_string(t);
 	cout<<s<<endl;
@@ -15,6 +32,19 @@ int main() {
 	string ss = to_iso_extended_string(tt);
 	cout<<ss<<endl;
 
-	cin.get();
-	return 0;
+	cout<<"[END Datetime]\n";
+}
+ 
+void testPropertyTree(){
+	cout<<"[START TEST PropertyTree]\n";
+	
+	try {
+		debug_settings ds;
+		ds.load("etc/Boost.PropertyTree.xml");
+		cout<<ds.m_level<<endl;
+	} catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << "\n";
+	}
+
+	cout<<"[START PropertyTree]\n";
 }
